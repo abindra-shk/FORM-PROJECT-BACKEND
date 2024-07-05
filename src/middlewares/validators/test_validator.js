fe458e25c279c312b42512857fdb4c987eaf6b94
Abindra Shakya <abindrashakya.123@gmail.com>
2024-07-05 08:51:26 +0545

    Modified firstname to name.

--- src/middlewares/validators/test_validator.js
+++ src/middlewares/validators/test_validator.js
@@ -1,12 +1,12 @@
-import Joi from "joi";
+import Joi from 'joi';
 
-import showValidationsError from "../../utils/display_validation_error.js";
+import showValidationsError from '../../utils/display_validation_error.js';
 
 export class TestValidator {
   static async createTest(req, res, next) {
     const schema = Joi.object({
-      firstName: Joi.string().trim().max(20),
-      lastName: Joi.string().trim().max(20),
+      name: Joi.string().trim().max(50),
+      // lastName: Joi.string().trim().max(20),
       email: Joi.string().trim().max(200),
       address: Joi.string().trim().max(60),
       ratePerHour: Joi.number(),
@@ -22,8 +22,8 @@ export class TestValidator {
 
   static async updateTest(req, res, next) {
     const schema = Joi.object({
-      firstName: Joi.string().trim().max(20),
-      lastName: Joi.string().trim().max(20),
+      name: Joi.string().trim().max(50),
+      // lastName: Joi.string().trim().max(20),
       email: Joi.string().trim().max(200),
       address: Joi.string().trim().max(60),
       ratePerHour: Joi.number(),
--- src/models/test.js
+++ src/models/test.js
@@ -1,83 +1,58 @@
-import mongoose from "mongoose";
-
+import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const testSchema = new Schema(
   {
-    firstName: {
+    name: {
       type: String,
-
-      default: "",
-
+      default: '',
       trim: true,
     },
-
-    lastName: {
-      type: String,
-
-      default: "",
-
-      trim: true,
-    },
-
+    // lastName: {
+    //   type: String,
+    //   default: '',
+    //   trim: true,
+    // },
     email: {
       type: String,
-
-      default: "",
-
+      default: '',
       trim: true,
     },
-
     address: {
       type: String,
-
-      default: "",
-
+      default: '',
       trim: true,
     },
-
     startDate: {
       type: Date,
     },
-
     endDate: {
       type: Date,
     },
-
     days: {
       type: Number,
     },
-
     ratePerHour: {
       type: Number,
 
       trim: true,
-
       default: 0,
     },
-
     hours: {
       type: Number,
-
       trim: true,
-
       default: 0,
     },
-
     total: {
       type: Number,
-
       trim: true,
-
       default: 0,
     },
   },
-
   {
     timestamps: true,
   }
 );
 
-const Test = mongoose.model("Test", testSchema);
-
+const Test = mongoose.model('Test', testSchema);
 export { Test };
